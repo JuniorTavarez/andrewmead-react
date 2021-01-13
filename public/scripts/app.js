8fbf75134bb5e664de654a7626d5778fc9c7a27e
juniortavarez <jtavarez8@gmail.com>
2021-01-13 16:45:54 -0500

    coditional Rendeing JSX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,67 @@
 'use strict';
 
+var appObject = {
+  title: 'Indicition App',
+  subtitle: 'Put your life in the hands of a computer',
+  options: ['one', 'two']
+};
+
 // JSX - Javascript XML
-
 var template = React.createElement(
-  'p',
+  'div',
   null,
-  'This is JSX from app.js ey check'
+  React.createElement(
+    'h1',
+    null,
+    appObject.title
+  ),
+  appObject.subtitle && React.createElement(
+    'p',
+    null,
+    ' ',
+    appObject.subtitle,
+    ' '
+  ),
+  React.createElement(
+    'p',
+    null,
+    appObject.options.length > 0 ? appObject.options[0] : appObject.options[1]
+  )
 );
-var domContainer = document.querySelector('#app');
 
-ReactDOM.render(template, domContainer);
+var user = {
+  name: 'Junior',
+  age: 18,
+  location: 'Jersey'
+};
+
+// function getLocation(location) {
+//   if (location) {
+//     return <p>Location: {location} </p>;
+//   }
+// }
+
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    user.name ? user.name : 'Anonymous'
+  ),
+  user.age && user.age >= 18 && React.createElement(
+    'p',
+    null,
+    'Age: ',
+    user.age
+  ),
+  React.createElement(
+    'p',
+    null,
+    user.location ? 'Location: ' + user.location : ''
+  )
+);
+
+var appRoot = document.querySelector('#app');
+
+ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,44 @@
 'use strict';
 
-// JSX - Javascript XML
-const template = <p>This is JSX from app.js ey check</p>;
-const domContainer = document.querySelector('#app');
+const appObject = {
+  title: 'Indicition App',
+  subtitle: 'Put your life in the hands of a computer',
+  options: ['one', 'two'],
+};
 
-ReactDOM.render(template, domContainer);
+// JSX - Javascript XML
+const template = (
+  <div>
+    <h1>{appObject.title}</h1>
+    {appObject.subtitle && <p> {appObject.subtitle} </p>}
+    <p>
+      {appObject.options.length > 0
+        ? appObject.options[0]
+        : appObject.options[1]}
+    </p>
+  </div>
+);
+
+const user = {
+  name: 'Junior',
+  age: 18,
+  location: 'Jersey',
+};
+
+// function getLocation(location) {
+//   if (location) {
+//     return <p>Location: {location} </p>;
+//   }
+// }
+
+const templateTwo = (
+  <div>
+    <h1>{user.name ? user.name : 'Anonymous'}</h1>
+    {user.age && user.age >= 18 && <p>Age: {user.age}</p>}
+    <p>{user.location ? 'Location: ' + user.location : ''}</p>
+  </div>
+);
+
+const appRoot = document.querySelector('#app');
+
+ReactDOM.render(template, appRoot);
