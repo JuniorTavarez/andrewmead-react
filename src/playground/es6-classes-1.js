1708f253b6ab2218ae070e750cf20dd1dccee491
juniortavarez <jtavarez8@gmail.com>
2021-01-19 18:58:39 -0500

    USING Class with ES6

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -4,7 +4,7 @@ class Person {
     this.age = age;
   }
   getGretting() {
-    return `Hi! ${this.name} `;
+    return `Hi. I am ${this.name} `;
   }
 
   getDescription() {
@@ -12,11 +12,39 @@ class Person {
   }
 }
 
-const me = new Person('Junior Tavarez', 32);
+class Student extends Person {
+  constructor(name, age, major) {
+    super(name, age);
+    this.major = major;
+  }
+  hasMajor() {
+    return !!this.major;
+  }
+  getDescription() {
+    let description = super.getDescription();
+    if (this.hasMajor()) {
+      description += `Their Major is ${this.major}`;
+    }
+    return description;
+  }
+}
 
+class Travaler extends Person {
+  constructor(name, age, homeLocation) {
+    super(name, age);
+    this.homeLocation = homeLocation;
+  }
+  getGretting() {
+    let gretting = super.getGretting();
+    if (this.homeLocation) {
+      gretting += `I'm visiting from ${this.homeLocation}`;
+    }
+    return gretting;
+  }
+}
+
+const me = new Travaler('Junior Tavarez', 32);
 console.log(me.getGretting());
-console.log(me.getDescription());
 
-const wife = new Person('Rebeca Tavarez');
-console.log(wife);
-console.log(wife.getDescription());
+const wife = new Travaler('Rebeca Tavarez', 26, 'New York');
+console.log(wife.getGretting());
